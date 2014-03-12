4bdcee8f812eb09ff353ea88eeb7e949891b8f2d
algspd <algspd@f52c84e1-ef97-1310-d76f-dda4c5f09345>
2014-03-12 15:26:55 +0000

    Añadidos varios micros

--- trunk/carpeta_proyecto/database/insert.sql
+++ trunk/carpeta_proyecto/database/insert.sql
@@ -1,2 +1,9 @@
-INSERT INTO micros VALUES ('ATmega8', '8-bit Atmel', '16', '8', '1');
+--                          Ref             Arch                freq    flash    ram         manufacturer
+INSERT INTO micros VALUES ('ATmega8',       '8-bit Atmel',      '16',   '8',     '1');    -- Atmel
+INSERT INTO micros VALUES ('PIC16F54',      'Harvard',          '20',   '0.75',  '0.2');  -- Microchip
+INSERT INTO micros VALUES ('MSP430',        '16-bit RISC',      '25',   '512',   '1');    -- Texas instruments
+INSERT INTO micros VALUES ('C8051F304-GM',  '8051',             '25',   '8',     '0.25'); -- Silabs
+INSERT INTO micros VALUES ('P8X32A-D40',    '32-bit Multicore', '12',   '64',    '32');   -- Parallax
+--INSERT INTO micros VALUES ('', '', '', '', ''); --
+
 
