2c0cefe088ed8441272c3441d80ac31b93dcd514
algspd <algspd@f52c84e1-ef97-1310-d76f-dda4c5f09345>
2014-03-12 23:45:26 +0000

    Añadido el controlador principal con su vista correspondiente para comenzar el desarrollo del marco comun

---
+++ trunk/src/application/controllers/main.php
@@ -0,0 +1,12 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Main extends CI_Controller {
+
+  /**
+   * Controlador principal, el que se carga al Index Page for this controller.
+   */
+  public function index() {
+    $this->load->view('main_view');
+  }
+}
+
---
+++ trunk/src/application/views/main_view.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>MicroSearch</title>
+
+	<style type="text/css">
+
+	body {
+		background-color: #fff;
+		margin: 40px;
+		font: 13px/20px normal Helvetica, Arial, sans-serif;
+		color: #4F5155;
+	}
+	</style>
+</head>
+<body>
+  <div id="container">
+    <h1>Página inicial de MicroSearch</h1>
+    <div id="body">
+      <p>Esta es la página inicial.</p>
+    </div>
+  </div>
+</body>
+</html>
