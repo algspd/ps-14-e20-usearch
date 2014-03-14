1349f6ee8712800a1374050ed2b8ebf4d5324d86
algspd <algspd@f52c84e1-ef97-1310-d76f-dda4c5f09345>
2014-03-14 16:21:25 +0000

    Controlador de edición de micros funcionando con autorellenado de campos, a falta de que el botón de "guardar" haga su efecto

--- src/application/controllers/editar.php
+++ src/application/controllers/editar.php
@@ -1,17 +1,21 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Listar_todo extends CI_Controller {
+class Editar extends CI_Controller {
 
   public function index() {
 
     $this->load->helper('url');
     $this->load->helper('html');
 
-    $this->load->model('microsmodel','',TRUE);
-    $def=$this->microsmodel->listar_uno("ATmega8");
-
-    $data = array('def'=> $def);
-    $this->load->view('editar',$data);
+    if ( isset($_POST['ref'])) {
+      $this->load->model('microsmodel','',TRUE);
+      $def=$this->microsmodel->listar_uno($_POST['ref']);
+      $data = array('def'=> $def);
+      $this->load->view('editar',$data);
+    }
+    else{
+      echo "Argumentos no correctos";
+    }
 
   }
 }
