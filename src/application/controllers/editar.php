9b70a1be4997bbebac64bdbc3aa865c461c43b2f
algspd <algspd@f52c84e1-ef97-1310-d76f-dda4c5f09345>
2014-03-14 16:44:33 +0000

    Actualizado editar, ya actualiza sobre base de datos

--- src/application/controllers/editar.php
+++ src/application/controllers/editar.php
@@ -6,15 +6,31 @@ class Editar extends CI_Controller {
 
     $this->load->helper('url');
     $this->load->helper('html');
+    $this->load->model('microsmodel','',TRUE);
 
-    if ( isset($_POST['ref'])) {
-      $this->load->model('microsmodel','',TRUE);
+    if (isset($_POST['ref']) && isset($_POST['arch']) &&
+        isset($_POST['freq']) && isset($_POST['flash']) &&
+        isset($_POST['ram']) && isset($_POST['precio']) ){
+      // Se ha enviado un micro completo para ser modificado
+      $this->microsmodel->modificar(
+        $_POST['ref'],$_POST['arch'],
+        $_POST['freq'],$_POST['flash'],
+        $_POST['ram'],$_POST['precio']);
+      echo "Micro modificado\n";
+      return;
+    }
+
+    if (isset($_POST['ref'])) {
+      // Se ha enviado solo la referencia, procedemos a hacer la modificación
       $def=$this->microsmodel->listar_uno($_POST['ref']);
       $data = array('def'=> $def);
       $this->load->view('editar',$data);
     }
-    else{
-      echo "Argumentos no correctos";
+    if (! isset($_POST['ref'])) {
+      // No hay datos en el post, cargamos la lista para que el usuario elija qué editar
+      $resultado=$this->microsmodel->listar_todo();
+      $data = array('resultado'=> $resultado);
+      $this->load->view('listar_todo_editar',$data);
     }
 
   }
