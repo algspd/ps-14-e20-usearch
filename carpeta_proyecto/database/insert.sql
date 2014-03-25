7ed3a65881a3f21688aac61c5da10ab9907def0f
daniegarcia254@gmail.com <daniegarcia254@gmail.com@f52c84e1-ef97-1310-d76f-dda4c5f09345>
2014-03-25 08:58:50 +0000

    Añadidos 3 microcontroladores

--- carpeta_proyecto/database/insert.sql
+++ carpeta_proyecto/database/insert.sql
@@ -4,6 +4,9 @@ INSERT INTO micros VALUES ('PIC16F54',      'Harvard',          '20',   '0.75', 
 INSERT INTO micros VALUES ('MSP430',        '16-bit RISC',      '25',   '512',   '1',   '3.45');  -- Texas instruments
 INSERT INTO micros VALUES ('C8051F304-GM',  '8051',             '25',   '8',     '0.25','7.25');  -- Silabs
 INSERT INTO micros VALUES ('P8X32A-D40',    '32-bit Multicore', '12',   '64',    '32',  '32.18'); -- Parallax
---INSERT INTO micros VALUES ('', '', '', '', ''); --
+INSERT INTO micros VALUES ('AT32UC3A0128', 	'32-bit AVR', 		'66', 	'128', 	 '32', 	'5.68');  -- Atmel
+INSERT INTO micros VALUES ('PIC32MX110', 	'32-bit RISC', 		'40', 	'32', 	 '4', 	'1.66');  -- Microchip
+INSERT INTO micros VALUES ('TMS320F28030', 	'32-bit Piccolo', 	'60', 	'32', 	 '12', 	'2.20');  -- Texas instruments
+--INSERT INTO micros VALUES ('', '', '', '', '', ''); --
 
 
