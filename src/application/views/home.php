32ad161c946d2ede03c79b1217ac24b15a031de6
algspd <algspd@f52c84e1-ef97-1310-d76f-dda4c5f09345>
2014-03-27 16:39:12 +0000

    Eliminado el canvas, adaptado a css

--- src/application/views/home.php
+++ src/application/views/home.php
@@ -1,20 +1,6 @@
 ﻿		</div>
 		<div id="titulo">
-			<canvas id="canvasTitulo" width="1000" height="60"></canvas>
-			<script>
-				var canvas = document.getElementById('canvasTitulo');
-				var context = canvas.getContext('2d');
-				var x = canvas.width / 2;
-				var y = canvas.height / 2;
-
-				context.font = '18pt Calibri bold';
-				// textAlign aligns text horizontally relative to placement
-				context.textAlign = 'center';
-				// textBaseline aligns text vertically relative to font style
-				context.textBaseline = 'middle';
-				context.fillStyle = 'blue';
-				context.fillText('!Bienvenido a nuestro catálogo!', x, y);
-			</script>
+                        ¡Bienvenido a nuestro catálogo!
 		</div>
 		<div>
 			<div id="descripcion" rows="20" cols="120" readonly="true">
