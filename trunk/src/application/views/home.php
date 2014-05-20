ed173f8961adf13a0ca780dbd6e5cf2a1326db44
taleklegend92 <taleklegend92@f52c84e1-ef97-1310-d76f-dda4c5f09345>
2014-05-20 16:26:23 +0000

    modificada la pagina home de la interfaz

--- trunk/src/application/views/home.php
+++ trunk/src/application/views/home.php
@@ -2,11 +2,12 @@
                             ¡Bienvenido a nuestro catálogo!
     		</div>
             <div id="descripcion" class="texto_centrado">
-					Somos una empresa que se dedica a la venta de microcontroladores de múltiples marcas.<br>
-					Mas texto.<br>
-					Mas texto.<br>
-					Mas texto.<br>
-					Mas texto.<br>
-					...<br>
+					Somos una empresa que se dedica a la venta de microcontroladores de múltiples marcas. <br>
+					Se dispone de una gran variedad de microcontroladores con diferentes arquitecturas,<br>
+					frecuencias, memorias flash, memorias Ram y lo más importante, posiblemente los mejores precios del mercado.<br>
+					<br>Se puede acceder facilmente al listado completo de microcontroldores, así como una fácil y rápida búsqueda por características.<br>
+					<br>
+					<br>
+					Disfrute de su compra!!
             </div>
 		</div> <!-- contenido ppal -->
