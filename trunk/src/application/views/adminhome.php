845275aa07ba8e00178c738143207a33855876d8
daniegarcia254@gmail.com <daniegarcia254@gmail.com@f52c84e1-ef97-1310-d76f-dda4c5f09345>
2014-05-20 16:57:21 +0000

    Descripción home admin

--- trunk/src/application/views/adminhome.php
+++ trunk/src/application/views/adminhome.php
@@ -2,6 +2,16 @@
                             ¡Bienvenido a la página de administración!
     		</div>
             <div id="descripcion" rows="20" cols="120" readonly="true">
-    				Desde aquí puedes gestionar los microcontroladores del catálogo: agregar nuevos, editar los existentes o eliminarlos
+    				<p style="padding-left:20px;">Desde esta página usted podrá realizar las siguientes funciones de admnistración sobre la base de datos de microcontroladores:
+					<ul>
+						<li>Añadir nuevos microcontroladores.</li>
+						<li>Acceder al listado completo de microcontroladores o realizar una búsqueda, desde donde podrá:</li>
+						<ul>
+							<li>Editar las características de un microcontrolador existente.</li>
+							<li>Eliminar un microcontrolador existente.</li>
+						</ul>
+						<li></li>
+					</ul>
+					</p>
             </div>
 		</div> <!-- contenido ppal -->
