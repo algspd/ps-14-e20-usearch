5e0b1bea67e1c81a10b0fdc7f799d25e98e05af9
daniegarcia254@gmail.com <daniegarcia254@gmail.com@f52c84e1-ef97-1310-d76f-dda4c5f09345>
2014-05-21 23:00:59 +0000

    Modificado texto de las páginas de inicio

--- src/application/views/adminhome.php
+++ src/application/views/adminhome.php
@@ -2,15 +2,17 @@
                             ¡Bienvenido a la página de administración!
     		</div>
             <div id="descripcion" rows="20" cols="120" readonly="true">
-    				<p style="padding-left:20px;">Desde esta página usted podrá realizar las siguientes funciones de admnistración sobre la base de datos de microcontroladores:
-					<ul>
+    				<p style="padding-left:20px;">Desde esta página usted como administrador podrá realizar las siguientes funciones 
+					de admnistración sobre la base de datos de microcontroladores de  µSearch:
+					<ul style="padding-left:100px; line-height: 1.5em;">
 						<li>Añadir nuevos microcontroladores.</li>
-						<li>Acceder al listado completo de microcontroladores o realizar una búsqueda, desde donde podrá:</li>
+						<li>Acceder al listado completo de microcontroladores</li>
+						<li>Realizar búsquedas más exhaustivas de microcontroladores que se le mostrarán en forma de listado.</li>
+						<li>Desde dichos listados, podrá:</li>
 						<ul>
 							<li>Editar las características de un microcontrolador existente.</li>
 							<li>Eliminar un microcontrolador existente.</li>
 						</ul>
-						<li></li>
 					</ul>
 					</p>
             </div>
